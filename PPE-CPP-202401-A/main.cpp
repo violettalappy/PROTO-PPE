7070f7a679788016813ba9e431c3e4d455b7e85a
hoanglongdummy <104409899+hoanglongplanner@users.noreply.github.com>
2024-01-19 16:30:08 +0700

    Add source codes

---
+++ PPE-CPP-202401-A/main.cpp
@@ -0,0 +1,10 @@
+﻿// Main entry point of the program
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	std::cout << "Program Performer Engine: INITIALIZE" << std::endl;
+	return 0;
+}
