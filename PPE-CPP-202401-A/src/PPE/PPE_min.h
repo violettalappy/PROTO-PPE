b8a54c889e1af6a02046945407f93a35349f4e31
hoanglongdummy <104409899+hoanglongplanner@users.noreply.github.com>
2024-01-22 08:23:36 +0700

    Update PPE_min.h

--- PPE-CPP-202401-A/src/PPE/PPE_min.h
+++ PPE-CPP-202401-A/src/PPE/PPE_min.h
@@ -22,24 +22,94 @@ Add all functions before using PPE.h
 #define PPE_H
 
 #include <iostream>
+#include <filesystem>
 #include <string>
+namespace fs = std::filesystem;
+
+// ? Convert variable class to string
+#define PPE_NAMEOF(name) #name
+
+#define K_PPE_VERSION "0.0.0-202401-A"
+
+#ifdef _WIN32
+#define K_PROGRAMCONFIG_FILEPATH "\config\config.xml"
+#elif defined(__linux__)
+#define K_PROGRAMCONFIG_FILEPATH "/config/config.xml"
+#endif
 
 class PPE {
 public:
-
+	static void LogInfo() {
+		std::string versionMSG = "Version = " K_PPE_VERSION;
+		PPE::Log(PPE_NAMEOF(PPE), versionMSG);
+		std::string pcfbMSG = "ProgramConfigPath = " + fs::current_path().string() + K_PROGRAMCONFIG_FILEPATH;
+		PPE::Log(PPE_NAMEOF(PPE), pcfbMSG);
+	}
+public:
 	/* LOGGER */
 	// ? Loging all program behaviors
 	// ! DO NOT LOG IN UPDATE EVERY FRAME
 	// TODO Design fallback system in case unable to write to log file
-
 	static void Log(std::string arg_title, std::string arg_msg, bool isIncludeTime = true) {
-		std::cout << "> LOG: " << arg_title << ": " << arg_msg << std::endl;
+		std::cout << "> " << arg_title << ": " << arg_msg << std::endl;
 	}
 	static void Warn(std::string arg_title, std::string arg_msg, bool isIncludeTime = true) {
 		std::cout << "> WARNING: " << arg_title << ": " << arg_msg << std::endl;
 	}
 	static void Fatal(std::string arg_title, std::string arg_msg, bool isIncludeTime = true) {
 		std::cout << "> FATAL: " << arg_title << ": " << arg_msg << std::endl;
+	}
+};
+
+class PPE_ProgramConfig {
+public:
+	PPE_ProgramConfig() {
+		bool isLoadConfigSuccess = false;
+		if (isLoadConfigSuccess) {
+		}
+		else {
+			PPE::Fatal("ProgramConfig", "load config fail, switch to fallback default !!");
+			SetProgramTitle("LearnWebGPU");
+			SetScreenWidth(800);
+			SetScreenHeight(600);
+			LogProgramConfigInfo();
+		}
+	}
+	~PPE_ProgramConfig() {
+	}
+	void LogProgramConfigInfo() {
+		std::string programTitleMSG = "ProgramTitle = " + GetProgramTitle();
+		PPE::Log(PPE_ProgramConfig::nameof(), programTitleMSG);
+		std::string screenWidthMSG = "ScreenWidth = " + std::to_string(GetScreenWidth());
+		PPE::Log(PPE_ProgramConfig::nameof(), screenWidthMSG);
+		std::string screenHeightMSG = "ScreenHeight = " + std::to_string(GetScreenHeight());
+		PPE::Log(PPE_ProgramConfig::nameof(), screenHeightMSG);
+	}
+private:
+	std::string _programTitle = "Insert program name here !!";
+	int _screenWidth = 800;
+	int _screenHeight = 600;
+public:
+	const std::string nameof() {
+		return "ProgramConfig";
+	}
+	std::string GetProgramTitle() {
+		return _programTitle;
+	}
+	int GetScreenWidth() {
+		return _screenWidth;
+	}
+	int GetScreenHeight() {
+		return _screenHeight;
+	}
+	void SetProgramTitle(std::string arg_name) {
+		_programTitle = arg_name;
+	}
+	void SetScreenWidth(int arg_value) {
+		_screenWidth = arg_value;
+	}
+	void SetScreenHeight(int arg_value) {
+		_screenHeight = arg_value;
 	}
 };
 
