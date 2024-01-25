4e70776f5f48fefad986e1471f657dcaa994f984
hoanglongdummy <104409899+hoanglongplanner@users.noreply.github.com>
2024-01-25 20:02:46 +0700

    Update main.cpp

--- PPE-CPP-202401-A/src/main.cpp
+++ PPE-CPP-202401-A/src/main.cpp
@@ -13,44 +13,12 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-#include <iostream>
-#include <GLFW/glfw3.h>
-#include <P3E/P3E_min.h>
-
-using namespace std;
+#include "ProgramContext.h"
 
 int main(int argc, char* argv[]) {
-	P3E::Logger::Info("INIT STAGE", P3E::KLoggerOwner::Program);
-
-	P3E::P3E_Init();
-	P3E::ProgramConfig programConfig = P3E::ProgramConfig();
-	if (!glfwInit()) {
-		std::cerr << "Could not initialize GLFW!" << std::endl;
-		return 1;
-	}
-	P3E::Logger::Info("init success !!", P3E::KLoggerOwner::GLFW);
-	GLFWwindow* window = glfwCreateWindow(programConfig.GetScreenWidth(), programConfig.GetScreenHeight(), programConfig.GetProgramFullTitle().c_str(), NULL, NULL);
-	if (!window) {
-		std::cerr << "Could not open window!" << std::endl;
-		glfwTerminate();
-		return 1;
-	}
-	P3E::Logger::Info("create window success !!", P3E::KLoggerOwner::GLFW);
-
-	P3E::Logger::Info("INIT SUCCESS !!", P3E::KLoggerOwner::Program);
-
-	P3E::Logger::Info("UPDATE STAGE", P3E::KLoggerOwner::Program);
-	while (!glfwWindowShouldClose(window)) {
-		glfwPollEvents();
-	}
-
-	P3E::Logger::Info("FREE RESOURCES STAGE", P3E::KLoggerOwner::Program);
-	glfwDestroyWindow(window);
-	glfwTerminate();
-	P3E::Logger::Info("FREE RESOURCES SUCCESS !!", P3E::KLoggerOwner::Program);
-
-	P3E::Logger::Info("SHUTDOWN STAGE", P3E::KLoggerOwner::Program);
-	P3E::Logger::Info("SHUTDOWN STAGE SUCCESS !!", P3E::KLoggerOwner::Program);
-
-	return 0;
+    ProgramContext program;
+    program.Init();
+    program.Run();
+    program.Close();
+    return 0;
 }
